03d48d9fdafb9f7ec9444857e1e8ca5e2f90a00f
Kyle Simpson <kes97@uw.edu>
2019-02-05 12:41:38 -0800

    Revise to meet assignment goals

--- src/components/FeatureBlock.jsx
+++ src/components/FeatureBlock.jsx
@@ -9,16 +9,23 @@ const FeatureBlock = () => (
                     <Card.Img />
                     <Card.Body>
                         <Card.Title>What is PlayTurnip?</Card.Title >
-                        <Card.Text>PlayTurnip is a platforming video game with a focus on an intimate story about mental health.</Card.Text>
+                        <Card.Text>PlayTurnip is a platforming video game with a focus on an intimate story about mental health.  The gaming community is large and, with little focus on mental health, we aim to bring conversations into the community using a familiar medium.</Card.Text>
                     </Card.Body>
                 </Card>
             </Col>
             <Col xs="6">
-                <Card>
+                {/* <Card>
                     <Card.Img />
                     <Card.Body>
                         <Card.Title>What can I expect?</Card.Title >
                         <Card.Text>You can expect a fun game narrating the story of our hero and their service animal Turnip.</Card.Text>
+                    </Card.Body>
+                </Card> */}
+                <Card>
+                    <Card.Img />
+                    <Card.Body>
+                        <Card.Title>Contact Us!</Card.Title >
+                        <Card.Text>With the game still in development, we want to hear your suggestions!  If you have experience developing in various game engines, with interactive storytelling, or game art design, hit us up on <a href="https://twitter.com/PlayTurnip" target="_blank">Twitter</a> or shoot us an <a href = "mailto: swartell@washington.edu">email</a>!</Card.Text>
                     </Card.Body>
                 </Card>
             </Col>
